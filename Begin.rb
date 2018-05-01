b6ba8c6f0f2be6e8c792c52a14d404255def0c4c
subba_gunjikunta <subba_gunjikunta@belk.com>
2018-05-01 09:33:15 -0400

    sample programs

---
+++ Begin.rb
@@ -0,0 +1,6 @@
+
+#Declares code to be called before the program is run.
+puts " this is second program"
+BEGIN {
+  puts "inside begin statement"
+}
---
+++ End.rb
@@ -0,0 +1,14 @@
+END {
+  puts "this should be invoked at the end of the program"
+}
+
+puts " let try this when this gets executed"
+BEGIN {
+  puts " this should execute first "
+}
+=begin
+This is a comment.
+This is a comment, too.
+This is a comment, too.
+I said that already.
+=end
