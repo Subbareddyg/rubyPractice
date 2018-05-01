89ab89c44eb776afc7add9c50b58f7774d502168
subba_gunjikunta <subba_gunjikunta@belk.com>
2018-04-30 20:46:11 -0400

    first code commit to get api service call in ruby

---
+++ getProduct.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'httparty'
+
+class GetProduct
+	include HTTParty
+	base_uri "https://app.salsify.com/api/orgs/s-8b629df0-ecc5-4bbf-8ff5-56a211460c08"
+	def getProduct
+		self.class.get('/products/18016271307805?access_token=22df23f1220b8e1af3d17f663229e5d338c51f92a53c4d30fc838d5508db5388')
+	end
+end
+
+GetProduct_product = GetProduct.new
+puts GetProduct_product.getProduct
